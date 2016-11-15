c455740b3128fa39a5b176f2dc2eb344bf124043
MikhailLU <clowfinger13@gmail.com>
2016-11-16 01:15:04 +0300

    scaffolds, registration make-up

--- code/stbarbershop/app/controllers/users_controller.rb
+++ code/stbarbershop/app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+    before_action :set_user
+
+    def new
+        @user = User.new
+    end
+
+    def index
+    end
+
+    def show
+    end
+
+    def destroy
+        @user.destroy
+        redirect_to root_path
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
+
+    def set_user
+        @user = User.find(params[:id])
+    end
 end
--- code/stbarbershop/config/routes.rb
+++ code/stbarbershop/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  resources :schedules
+  resources :meetings
+  resources :news
+  devise_for :users, :controllers => {registrations: 'registrations'}
+	resources :users
 
   root "mainpage#welcome"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
